13d0819e4969b8db57955a8ad7b18e516555fa76
Daniel Rodrigues <danielsr@gmail.com>
2018-07-21 17:49:20 -0300

    Add Apollo middleware to handle token

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,14 +1,27 @@
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
-
-const uri = process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:3000/graphql'
-
-const link = new HttpLink({ uri })
+import { ApolloClient, HttpLink, InMemoryCache, ApolloLink, concat } from 'apollo-client-preset'
 
 const cache = new InMemoryCache()
 
+const uri = process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:3000/graphql'
+
+const httpLink = new HttpLink({ uri })
+
+const token = localStorage.getItem('token')
+
+const middleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-access-token': token,
+    }
+  }))
+
+  return forward(operation);
+})
+
 const client = new ApolloClient({
   cache,
-  link
+  link: concat(middleware, httpLink)
 })
 
 export default client
