ca53b71b382bccd8532c2d1ce259225ea76f884f
Daniel Rodrigues <danielsr@gmail.com>
2018-07-22 19:39:01 -0300

    Form render with func or node

--- client/src/core/components/Form/index.jsx
+++ client/src/core/components/Form/index.jsx
@@ -6,9 +6,9 @@ const Form = ({ onSubmit, validate, children }) => (
   <FinalForm
     onSubmit={onSubmit}
     validate={validate}
-    render={({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        {children}
+    render={form => (
+      <form onSubmit={form.handleSubmit}>
+        {typeof children === 'function' ? children(form) : children}
       </form>
     )}
   />
@@ -17,7 +17,7 @@ const Form = ({ onSubmit, validate, children }) => (
 Form.propTypes = {
   onSubmit: PropTypes.func,
   validate: PropTypes.func,
-  children: PropTypes.node
+  children: PropTypes.any
 }
 
 export default Form
